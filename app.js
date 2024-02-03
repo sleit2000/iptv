ee4d19916637eb0dc50300d3b3bd3fd172e52221
sleit2000 <93011124+sleit2000@users.noreply.github.com>
2024-02-04 00:31:22 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// Включване на модули
+const module1 = require('./index.js');
+const module2 = require('./_env');
+const module3 = require('./package-lock.json');
+const module3 = require('./package.json');
+// Използване на модулите
+module1.method1();
+module2.method2();
+module3.method3();
+module4.method3();
